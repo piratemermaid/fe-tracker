f62d3782738f52e5e2790602d936349afc5793a8
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-05-04 19:00:10 -0400

    change ports

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8001;
 app.listen(port);
 console.log("app listening on port " + port);
 
