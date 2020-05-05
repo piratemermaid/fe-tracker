e71017131c05911afd9ed0861ddc3204e2f256a5
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-05-05 17:08:16 -0400

    update roster ui

--- client/src/components/RosterRow.js
+++ client/src/components/RosterRow.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { getNextClass } from "../helpers/helpers";
 import { displayClassSkills } from "../helpers/uihelpers";
 
+import Grid from "@material-ui/core/Grid";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+
 const RosterRow = (props) => {
     const { name, classes, skills } = props.student;
     const nextClass = getNextClass(classes);
@@ -19,22 +22,33 @@ const RosterRow = (props) => {
 
     return (
         <div key={name} className="roster-row">
-            <div className="roster-img"></div>
-            {name}
-            <br />
-            {classes.length > 0 ? (
-                <span>Next class: {renderClass(nextClass)}</span>
-            ) : (
-                "No classes set"
-            )}
-            <br />
-            {nextClass ? (
-                <span>
-                    Skills needed: {displayClassSkills(nextClass.classSkills)}
-                </span>
-            ) : null}
-            <br />
-            <Link to={`/student/${name}`}>Update</Link>
+            <Grid container spacing={3}>
+                <Grid item xs={3}>
+                    <div className="roster-img"></div>
+                </Grid>
+                <Grid item xs={7}>
+                    {name}
+                    <br />
+                    {classes.length > 0 ? (
+                        <span>Next class: {renderClass(nextClass)}</span>
+                    ) : (
+                        "No classes set"
+                    )}
+                    <br />
+                    {nextClass ? (
+                        <span>
+                            Skills needed:{" "}
+                            {displayClassSkills(nextClass.classSkills)}
+                        </span>
+                    ) : null}
+                    <br />
+                </Grid>
+                <Grid item xs={2}>
+                    <Link to={`/student/${name}`}>
+                        <ArrowRightAltIcon />
+                    </Link>
+                </Grid>
+            </Grid>
         </div>
     );
 };
