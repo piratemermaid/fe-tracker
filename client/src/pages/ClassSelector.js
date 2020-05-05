9d6c3b890f855b6ae431945d3fbb3ea21a3304ba
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-05-05 17:26:18 -0400

    dry code

--- client/src/pages/ClassSelector.js
+++ client/src/pages/ClassSelector.js
@@ -25,41 +25,31 @@ class ClassSelector extends Component {
 
     render() {
         const { name } = this.props.match.params;
+
+        const types = [
+            "Beginner",
+            "Intermediate",
+            "Advanced",
+            "Master",
+            "Unique"
+        ];
+
         return (
             <div>
                 <h1>Select Classes for {name}</h1>
-                <ClassSelectorType
-                    type="Beginner"
-                    student={name}
-                    classes={_.filter(this.state.classes, { type: "Beginner" })}
-                    selectClassGoal={this.props.selectClassGoal}
-                />
-                <ClassSelectorType
-                    type="Intermediate"
-                    student={name}
-                    classes={_.filter(this.state.classes, {
-                        type: "Intermediate"
-                    })}
-                    selectClassGoal={this.props.selectClassGoal}
-                />
-                <ClassSelectorType
-                    type="Advanced"
-                    student={name}
-                    classes={_.filter(this.state.classes, { type: "Advanced" })}
-                    selectClassGoal={this.props.selectClassGoal}
-                />
-                <ClassSelectorType
-                    type="Master"
-                    student={name}
-                    classes={_.filter(this.state.classes, { type: "Master" })}
-                    selectClassGoal={this.props.selectClassGoal}
-                />
-                <ClassSelectorType
-                    type="Unique"
-                    student={name}
-                    classes={_.filter(this.state.classes, { type: "Unique" })}
-                    selectClassGoal={this.props.selectClassGoal}
-                />
+                {types.map((type) => {
+                    return (
+                        <ClassSelectorType
+                            type={type}
+                            student={name}
+                            classes={_.filter(this.state.classes, {
+                                type: "Beginner"
+                            })}
+                            selectClassGoal={this.props.selectClassGoal}
+                            key={type}
+                        />
+                    );
+                })}
             </div>
         );
     }
