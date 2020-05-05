55fe52ece36a67ab59f9536f7343854164331150
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-05-05 17:25:14 -0400

    add accordions for class types

--- client/src/components/ClassSelectorType.js
+++ client/src/components/ClassSelectorType.js
@@ -1,51 +1,71 @@
 import React from "react";
 import { displayClassSkills } from "../helpers/uihelpers";
 
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 const ClassSelectorType = (props) => {
     return (
         <div>
-            <h2>{props.type}</h2>
-            <ul>
-                {props.classes.map(
-                    ({
-                        name,
-                        skills,
-                        abilities,
-                        mastery_ability,
-                        mastery_combat_art
-                    }) => {
-                        return (
-                            <li
-                                key={name}
-                                style={{ marginBottom: "16px" }}
-                                onClick={() =>
-                                    props.selectClassGoal({
-                                        studentName: props.student,
-                                        className: name
-                                    })
-                                }
-                            >
-                                {name}
-                                <br />
-                                Skills required: {displayClassSkills(skills)}
-                                <br />
-                                Class abilities: {abilities || "none"}
-                                <br />
-                                Class mastery:{" "}
-                                {mastery_ability ? (
-                                    <span>Ability {mastery_ability}</span>
-                                ) : null}
-                                {mastery_ability && mastery_combat_art
-                                    ? ", "
-                                    : null}
-                                {mastery_combat_art ? (
-                                    <span>Combat Art {mastery_combat_art}</span>
-                                ) : null}
-                            </li>
-                        );
-                    }
-                )}
-            </ul>
+            <ExpansionPanel>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    {props.type}
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <ul>
+                        {props.classes.map(
+                            ({
+                                name,
+                                skills,
+                                abilities,
+                                mastery_ability,
+                                mastery_combat_art
+                            }) => {
+                                return (
+                                    <li
+                                        key={name}
+                                        style={{ marginBottom: "16px" }}
+                                        onClick={() =>
+                                            props.selectClassGoal({
+                                                studentName: props.student,
+                                                className: name
+                                            })
+                                        }
+                                    >
+                                        {name}
+                                        <br />
+                                        Skills required:{" "}
+                                        {displayClassSkills(skills)}
+                                        <br />
+                                        Class abilities: {abilities || "none"}
+                                        <br />
+                                        Class mastery:{" "}
+                                        {mastery_ability ? (
+                                            <span>
+                                                Ability {mastery_ability}
+                                            </span>
+                                        ) : null}
+                                        {mastery_ability && mastery_combat_art
+                                            ? ", "
+                                            : null}
+                                        {mastery_combat_art ? (
+                                            <span>
+                                                Combat Art {mastery_combat_art}
+                                            </span>
+                                        ) : null}
+                                    </li>
+                                );
+                            }
+                        )}
+                    </ul>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
         </div>
     );
 };
