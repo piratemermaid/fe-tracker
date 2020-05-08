364d17e40630b815dd821d94eba3e376a31962f9
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-05-08 09:53:10 -0400

    new build

---
+++ client/build/precache-manifest.bc34dd0efe1c8c20483eba8644ee8f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b42f0742dc0e05e8c5e1764787d02b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "045e5a6ebb1adeea924b",
+    "url": "/static/css/main.31f40dc6.chunk.css"
+  },
+  {
+    "revision": "fd5716a968844af9235e",
+    "url": "/static/js/2.24cbfe49.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.24cbfe49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "045e5a6ebb1adeea924b",
+    "url": "/static/js/main.8ea55103.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
