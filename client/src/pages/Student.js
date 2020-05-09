d448df527e83785c7f8d46aa63ce4a815f28a606
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-05-08 21:11:05 -0400

    put padding back on student page

--- client/src/pages/Student.js
+++ client/src/pages/Student.js
@@ -189,7 +189,7 @@ const Student = (props) => {
     };
 
     return (
-        <div style={{ width: "100%" }}>
+        <div style={{ width: "100%" }} className="padding">
             <Link to="/">
                 <KeyboardBackspaceIcon />
             </Link>
