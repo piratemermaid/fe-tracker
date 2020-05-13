dae7bd1740b72008c1e19bad34bcaa3aea71cb2b
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-05-13 13:12:10 -0400

    add skill filters to add students page

--- client/src/pages/AddStudent.js
+++ client/src/pages/AddStudent.js
@@ -2,6 +2,7 @@ import _ from "lodash";
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { stripSpaces } from "../helpers/helpers";
 
 import Loading from "../components/Loading";
 import StudentImg from "../components/StudentImg";
@@ -9,12 +10,31 @@ import BackButton from "../components/BackButton";
 import SkillsOverview from "../components/SkillsOverview";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+
+const skills = [
+    "Sword",
+    "Lance",
+    "Axe",
+    "Bow",
+    "Brawl",
+    "Reason",
+    "Faith",
+    "Authority",
+    "Heavy Armor",
+    "Riding",
+    "Flying"
+];
 
 class AddStudent extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { availableStudents: [], selectedStudents: [] };
+        this.state = {
+            availableStudents: [],
+            selectedStudents: [],
+            filters: []
+        };
     }
 
     addStudent(name) {
@@ -28,7 +48,32 @@ class AddStudent extends Component {
         this.setState({ selectedStudents });
     }
 
+    onFilterChange(name) {
+        let { filters } = this.state;
+        if (filters.includes(name)) {
+            const index = _.indexOf(filters, name);
+            filters.splice(index, 1);
+        } else {
+            filters.push(name);
+        }
+        this.setState({ filters });
+    }
+
+    studentMeetsFilterReq(skills) {
+        const { filters } = this.state;
+
+        for (let skill of skills) {
+            const { name, proficient, budding } = skill;
+            if (filters.includes(name) && (proficient || budding)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     renderAvailableStudents() {
+        const { filters } = this.state;
         const { availableStudents, selectedStudents } = this.state;
 
         let studentList = [];
@@ -45,35 +90,37 @@ class AddStudent extends Component {
                 );
             }
 
-            studentList.push(
-                <Grid
-                    container
-                    className={`roster-row add-student-row${
-                        selectedStudents.includes(name)
-                            ? ` add-student-selected ${house.replace(
-                                  /\s+/g,
-                                  ""
-                              )}`
-                            : null
-                    }`}
-                    key={name}
-                    onClick={() => this.addStudent(name)}
-                >
-                    <Grid item xs={4}>
-                        <StudentImg
-                            name={name}
-                            byleth_gender={gender || "F"}
-                            house={house}
-                        />
+            if (filters.length < 1 || this.studentMeetsFilterReq(skills)) {
+                studentList.push(
+                    <Grid
+                        container
+                        className={`roster-row add-student-row${
+                            selectedStudents.includes(name)
+                                ? ` add-student-selected ${house.replace(
+                                      /\s+/g,
+                                      ""
+                                  )}`
+                                : null
+                        }`}
+                        key={name}
+                        onClick={() => this.addStudent(name)}
+                    >
+                        <Grid item xs={4}>
+                            <StudentImg
+                                name={name}
+                                byleth_gender={gender || "F"}
+                                house={house}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <p className="roster-name" style={{ marginTop: 0 }}>
+                                {name}
+                            </p>
+                            <SkillsOverview skills={skills} />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <p className="roster-name" style={{ marginTop: 0 }}>
-                            {name}
-                        </p>
-                        <SkillsOverview skills={skills} />
-                    </Grid>
-                </Grid>
-            );
+                );
+            }
         }
 
         return studentList;
@@ -102,7 +149,7 @@ class AddStudent extends Component {
     }
 
     render() {
-        const { selectedStudents } = this.state;
+        const { selectedStudents, filters } = this.state;
         if (!this.props.playthrough || !this.props.playthrough.students) {
             return <Loading />;
         }
@@ -111,6 +158,32 @@ class AddStudent extends Component {
             <div>
                 <BackButton />
                 <h1 className="padding">Add Students</h1>
+                <div className="padding skill-filters">
+                    {skills.map((name) => {
+                        return (
+                            <img
+                                key={name}
+                                onClick={() => this.onFilterChange(name)}
+                                className={`skill-filter${
+                                    filters.includes(name)
+                                        ? " filter-selected"
+                                        : ""
+                                }`}
+                                src={`/img/skills/${stripSpaces(name)}.png`}
+                                alt={name}
+                                title={name}
+                            />
+                        );
+                    })}
+                    {filters.length > 0 ? (
+                        <HighlightOffIcon
+                            className="hover remove-filters-btn"
+                            onClick={() => {
+                                this.setState({ filters: [] });
+                            }}
+                        />
+                    ) : null}
+                </div>
                 <ul>{this.renderAvailableStudents()}</ul>
                 <div className="center footer">
                     <Link to="/">
