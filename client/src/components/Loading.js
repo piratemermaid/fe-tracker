0d5a5616c947b8cdbbdbc3868c620b1017c788a2
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-05-18 16:28:27 -0400

    add loading spinner

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+import Grid from "@material-ui/core/Grid";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: "100px"
+    }
+}));
+
 const Loading = () => {
-    return <div id="App">loading...</div>;
+    const classes = useStyles();
+
+    return (
+        <Grid container justify="center">
+            <Grid item>
+                <div className={classes.root}>
+                    <CircularProgress size={60} thickness={4} />
+                </div>
+            </Grid>
+        </Grid>
+    );
 };
 
 export default Loading;
