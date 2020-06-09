f27b048305839b4233b7e61a35aca8de6c18e12d
schnur.ariel@gmail.com <schnur.ariel@gmail.com>
2020-06-09 13:27:50 -0400

    add all characters to gifts page

--- client/src/pages/Gifts.js
+++ client/src/pages/Gifts.js
@@ -15,51 +15,68 @@ const Gifts = (props) => {
 
     const userStudents = playthrough.students;
 
+    function renderGifts(students) {
+        return (
+            <div>
+                {students.map(({ name }) => {
+                    if (name !== "Byleth") {
+                        const appStudent = _.find(appStudents, { name });
+                        const { gifts } = appStudent;
+
+                        return (
+                            <div key={name} className="roster-row">
+                                <Grid container spacing={3} alignItems="top">
+                                    <Grid item xs={3}>
+                                        <StudentImg
+                                            name={name}
+                                            byleth_gender={
+                                                props.playthrough.byleth_gender
+                                            }
+                                            house={
+                                                name !== "Byleth"
+                                                    ? appStudent.house
+                                                    : props.playthrough.house
+                                            }
+                                        />
+                                    </Grid>
+                                    <Grid item xs={9}>
+                                        <p
+                                            className="roster-name"
+                                            style={{ marginTop: 0 }}
+                                        >
+                                            {name}
+                                        </p>
+                                        <ul className="styled-list">
+                                            {gifts.map(({ name }) => {
+                                                return (
+                                                    <li key={name}>{name}</li>
+                                                );
+                                            })}
+                                        </ul>
+                                    </Grid>
+                                </Grid>
+                            </div>
+                        );
+                    }
+                })}
+            </div>
+        );
+    }
+
     return (
         <div>
             <div className="padding">
                 <BackButton url="/info" />
                 <h2>Gifts</h2>
             </div>
-            {userStudents.map(({ name }) => {
-                if (name !== "Byleth") {
-                    const appStudent = _.find(appStudents, { name });
-                    const { gifts } = appStudent;
-
-                    return (
-                        <div key={name} className="roster-row">
-                            <Grid container spacing={3} alignItems="top">
-                                <Grid item xs={3}>
-                                    <StudentImg
-                                        name={name}
-                                        byleth_gender={
-                                            props.playthrough.byleth_gender
-                                        }
-                                        house={
-                                            name !== "Byleth"
-                                                ? appStudent.house
-                                                : props.playthrough.house
-                                        }
-                                    />
-                                </Grid>
-                                <Grid item xs={9}>
-                                    <p
-                                        className="roster-name"
-                                        style={{ marginTop: 0 }}
-                                    >
-                                        {name}
-                                    </p>
-                                    <ul className="styled-list">
-                                        {gifts.map(({ name }) => {
-                                            return <li key={name}>{name}</li>;
-                                        })}
-                                    </ul>
-                                </Grid>
-                            </Grid>
-                        </div>
-                    );
-                }
-            })}
+            <div className="padding">
+                <h3>Students in House</h3>
+            </div>
+            {renderGifts(userStudents)}
+            <div className="padding">
+                <h3>Everyone</h3>
+            </div>
+            {renderGifts(appStudents)}
         </div>
     );
 };
